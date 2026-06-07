4879ae0014132914704d68135197615136403262
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-06-07 15:48:17 +0200

    Implement user validation and detergent usage calculation
    Add functions to validate user schema and calculate last load date.

--- 06-2026.py
+++ 06-2026.py
@@ -162,7 +162,44 @@ UserProfile = {
 • The question mark (?) after supporter means that the field is optional, but is the specified type if it exists.
 • UserProfile[] denotes an array of UserProfile objects. An empty array is valid.
 • Extra keys are allowed """
+
+def check_obj(obj):
+    roles=["user","creator","moderator","staff","admin"]
+    k=list(obj.keys())
+    if not "username" in k or not "posts" in k or not "verified" in k or not "role" in k or not "badges" in k:
+         return False
+    ind1=k.index("username")
+    c1=isinstance(obj[k[ind1]],str)
+    ind2=k.index("posts")
+    c2=isinstance(obj[k[ind2]],int) and not isinstance(obj[k[ind2]],bool)
+    ind3=k.index("verified")
+    c3=isinstance(obj[k[ind3]], bool)
+    if "supporter" in k:
+        ind4=k.index("supporter")
+        c4=isinstance(obj[k[ind4]],bool)
+    for i in range(len(obj["badges"])):
+        if not isinstance(obj["badges"][i],str):
+            return False
+    if "supporter" in k:
+        return c1 and c2 and c3 and c4 and obj["role"] in roles
+    else:
+        return c1 and c2 and c3 and obj["role"] in roles
+
+def is_valid_schema(obj):
+    if not isinstance(obj["users"],list):
+       return False
+    elif obj["users"]==[]:
+       return True
+    else:
+       for i in range(len(obj["users"])):
+        if not check_obj(obj["users"][i]):
+            return False
+    return True
+
 """ 07-06-2026: Last Load
 Given the number of scoops of laundry detergent you have remaining and an array of how many scoops you used in each of the previous days, return the number of full days of detergent you have remaining.
 Calculate your average daily usage from the usage history and assume that amount of usage each day going forward. """
 
+import math
+def last_load_date(scoops, usage):
+    return math.floor(scoops/(sum(usage)/len(usage)))
