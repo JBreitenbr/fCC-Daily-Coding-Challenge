77c9042804520e56a2e3960ebb6f9b42428392f6
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-08-17 14:54:22 +0200

    Add space_jam function to process strings
    Implemented space_jam function to format strings.

--- 08-2026.py
+++ 08-2026.py
@@ -253,6 +253,10 @@ def fibonacci_sequence(start_seq, length):
 """ 14-08-2026: S P A C E J A M
 Given a string, remove all spaces from the string, insert two spaces between every character, convert all alphabetical letters to uppercase, and return the result.
 • Non-alphabetical characters should remain unchanged (except for spaces). """
+
+def space_jam(s):
+    return "  ".join(s.replace(" ","").upper())
+
 """ 15-08-2026: Jbelmud Text
 Given a string, return a jumbled version of that string where each word is transformed using the following constraints:
 • The first and last letters of the words remain in place
