fa38a4a29e60cce6c934348f76922cde326fc5c9
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-08-17 15:06:19 +0200

    Implement anagram checker function
    Added a function to check if two strings are anagrams, ignoring casing and whitespace.

--- 08-2026.py
+++ 08-2026.py
@@ -269,6 +269,10 @@ def jbelmu(text):
 """ 16-08-2026: Anagram Checker
 Given two strings, determine if they are anagrams of each other (contain the same characters in any order).
 • Ignore casing and white space. """
+
+def are_anagrams(str1, str2):
+    return sorted(list(str1.replace(" ","").lower()))==sorted(list(str2.replace(" ","").lower()))
+
 """ 17-08-2026: Targeted Sum
 Given an array of numbers and an integer target, find two unique numbers in the array that add up to the target value. Return an array with the indices of those two numbers, or "Target not found" if no two numbers sum up to the target.
 • The returned array should have the indices in ascending order. """
