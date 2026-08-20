46a995d41d9b5c0db0e4c509201498fd89a0572a
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-08-20 10:09:16 +0200

    Add function to count squares containing digit '3'

--- 08-2026.py
+++ 08-2026.py
@@ -322,3 +322,5 @@ def sum_of_squares(n):
 """ 20-08-2026: 3 Strikes
 Given an integer between 1 and 10,000, return a count of how many numbers from 1 up to that integer whose square contains at least one digit 3. """
 
+def squares_with_three(n):
+    return len([str(i*i) for i in range(1,n+1) if "3" in str(i*i)])
